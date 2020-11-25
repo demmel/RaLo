3094a49d29985b16c776d2c831e3e33597299c09
David Emmel <dgemmel2@gmail.com>
2020-11-24 20:31:56 -0800

    Clean up app state handling

--- src/main/index.ts
+++ src/main/index.ts
@@ -46,73 +46,22 @@ type AppState =
   | AppComposingState
   | AppClosingState;
 
-type AppInitEvent = {
-  type: "init";
-};
-
-type AppOnboardingEvent =
-  | {
-      type: "onboarding_xout";
-    }
-  | {
-      type: "onboarding_complete";
-      path: string;
-    };
-
-type AppRunningEvent = { type: "open_composer" };
-
-type AppComposingEvent =
-  | { type: "close_composer" }
-  | { type: "create_log"; text: string };
-
-type AppEvent =
-  | AppInitEvent
-  | AppOnboardingEvent
-  | AppRunningEvent
-  | AppComposingEvent;
-
 let appState: AppState = {
   type: "init",
 };
 
-function handleAppEvent(event: AppEvent) {
-  console.log("Event: ", event);
-  appState = reduce(appState, event);
-  console.log("State:", appState);
-}
+app.whenReady().then(() => {
+  init();
+});
 
-function reduce(state: AppState, event: AppEvent): AppState {
-  switch (event.type) {
-    case "init":
-      assertStateType(state, "init", event.type);
-      return reduceInitState(state, event);
-    case "onboarding_xout":
-    case "onboarding_complete":
-      assertStateType(state, "onboarding", event.type);
-      return reduceOnboardingState(state, event);
-    case "open_composer":
-      assertStateType(state, "running", event.type);
-      return reduceRunningState(state, event);
-    case "close_composer":
-    case "create_log":
-      assertStateType(state, "composing", event.type);
-      return reduceComposingState(state, event);
-  }
-}
+// Don't stop when the windows close
+app.on("window-all-closed", (e: Event) => e.preventDefault());
 
-function assertStateType<T extends AppState["type"]>(
-  state: AppState,
-  expected: T,
-  eventType: AppEvent["type"]
-): asserts state is Extract<AppState, { type: T }> {
-  if (state.type != expected) {
-    throw new Error(
-      `Event '${eventType}' cannot be fired in the '${state.type}' state`
-    );
-  }
-}
+// Unregister the shortcut so we don't keep it when we no longer need it
+app.on("will-quit", () => globalShortcut.unregisterAll());
 
-function reduceInitState(state: AppInitState, event: AppInitEvent): AppState {
+function init() {
+  assertStateType(appState, "init");
   const window = new BrowserWindow({
     webPreferences: { enableRemoteModule: true, nodeIntegration: true },
     width: 800,
@@ -128,124 +77,27 @@ function reduceInitState(state: AppInitState, event: AppInitEvent): AppState {
   maybeEnableWindowDevMode(window);
 
   window.on("closed", () => {
-    handleAppEvent({ type: "onboarding_xout" });
+    closeApp();
   });
 
   ipcMain.once("onboarding-complete", (event, result) => {
-    const path: string = result.path;
-    handleAppEvent({
-      type: "onboarding_complete",
-      path,
-    });
+    completeOnboarding(result.path);
   });
 
-  return {
+  appState = {
     type: "onboarding",
     window,
   };
 }
 
-function reduceOnboardingState(
-  state: AppOnboardingState,
-  event: AppOnboardingEvent
-): AppState {
-  switch (event.type) {
-    case "onboarding_xout":
-      return close();
-    case "onboarding_complete":
-      state.window.removeAllListeners();
-      closeWindow(state.window);
-      return startRunning({
-        path: event.path,
-      });
-  }
-}
-
-function close(): AppClosingState {
-  app.exit();
-  return {
-    type: "closing",
-  };
-}
-
-function startRunning(state: Omit<AppRunningState, "type" | "tray">): AppState {
-  const tray = new Tray(path.join(__static, "tray.png"));
-  const menu = Menu.buildFromTemplate([{ role: "quit" }]);
-  tray.setToolTip("RaLo");
-  tray.setContextMenu(menu);
-
-  if (
-    !globalShortcut.register("CmdOrCtrl+\\", () =>
-      handleAppEvent({ type: "open_composer" })
-    )
-  ) {
-    // TODO: Notify the user
-    return close();
-  }
-  return { ...state, type: "running", tray };
-}
-
-function reduceRunningState(
-  state: AppRunningState,
-  event: AppRunningEvent
-): AppState {
-  switch (event.type) {
-    case "open_composer":
-      const window = new BrowserWindow({
-        webPreferences: { enableRemoteModule: true, nodeIntegration: true },
-        width: 800,
-        height: 72,
-        useContentSize: true,
-        frame: false,
-      });
-
-      window.on("closed", () => {
-        handleAppEvent({ type: "close_composer" });
-      });
-
-      ipcMain.on("resize", (e, bounds) => {
-        window.setSize(800, bounds.height, false);
-        window.center();
-      });
-
-      ipcMain.once("create_log", (_, { text }) => {
-        handleAppEvent({ type: "create_log", text });
-      });
-
-      const url = getRouteURL("composer");
-      window.loadURL(url);
-
-      maybeEnableWindowDevMode(window);
-
-      return {
-        ...state,
-        type: "composing",
-        window,
-      };
-  }
-}
-
-function reduceComposingState(
-  state: AppComposingState,
-  event: AppComposingEvent
-): AppState {
-  switch (event.type) {
-    case "close_composer":
-      ipcMain.removeAllListeners();
-      return {
-        type: "running",
-        path: state.path,
-        tray: state.tray,
-      };
-    case "create_log":
-      ipcMain.removeAllListeners();
-      state.window.removeAllListeners();
-      closeWindow(state.window);
-      return {
-        type: "running",
-        path: state.path,
-        tray: state.tray,
-      };
+function assertStateType<T extends AppState["type"]>(
+  state: AppState,
+  expected: T
+): asserts state is Extract<AppState, { type: T }> {
+  if (state.type != expected) {
+    throw new Error(
+      `Event '${assertStateType.caller.name}' expected '${expected}' state type but state type is currently '${state.type}'`
+    );
   }
 }
 
@@ -264,15 +116,80 @@ function maybeEnableWindowDevMode(window: BrowserWindow) {
   });
 }
 
-app.whenReady().then(() => {
-  // handleAppEvent({ type: "init" });
+function closeApp() {
+  app.exit();
+  appState = {
+    type: "closing",
+  };
+}
 
-  appState = startRunning({ path: "." });
-  handleAppEvent({ type: "open_composer" });
-});
+function completeOnboarding(path: string) {
+  assertStateType(appState, "onboarding");
+  appState.window.removeAllListeners();
+  closeWindow(appState.window);
+  startRunning({
+    path,
+  });
+}
 
-// Don't stop when the windows close
-app.on("window-all-closed", (e: Event) => e.preventDefault());
+function startRunning(state: Omit<AppRunningState, "type" | "tray">) {
+  const tray = new Tray(path.join(__static, "tray.png"));
+  const menu = Menu.buildFromTemplate([{ role: "quit" }]);
+  tray.setToolTip("RaLo");
+  tray.setContextMenu(menu);
 
-// Unregister the shortcut so we don't keep it when we no longer need it
-app.on("will-quit", () => globalShortcut.unregisterAll());
+  if (!globalShortcut.register("CmdOrCtrl+\\", () => openComposer())) {
+    return closeApp();
+  }
+
+  appState = { ...state, type: "running", tray };
+}
+
+function openComposer() {
+  assertStateType(appState, "running");
+  const window = new BrowserWindow({
+    webPreferences: { enableRemoteModule: true, nodeIntegration: true },
+    width: 800,
+    height: 72,
+    useContentSize: true,
+    frame: false,
+  });
+
+  ipcMain.on("resize", (e, bounds) => {
+    window.setSize(800, bounds.height, false);
+    window.center();
+  });
+
+  ipcMain.once("create_log", (_, { text }) => {
+    assertStateType(appState, "composing");
+    ipcMain.removeAllListeners();
+    appState.window.removeAllListeners();
+    closeWindow(appState.window);
+    appState = {
+      type: "running",
+      path: appState.path,
+      tray: appState.tray,
+    };
+  });
+
+  window.on("closed", () => {
+    assertStateType(appState, "composing");
+    ipcMain.removeAllListeners();
+    appState = {
+      type: "running",
+      path: appState.path,
+      tray: appState.tray,
+    };
+  });
+
+  const url = getRouteURL("composer");
+  window.loadURL(url);
+
+  maybeEnableWindowDevMode(window);
+
+  appState = {
+    ...appState,
+    type: "composing",
+    window,
+  };
+}
